3d3502e7f123934aff897e2ea05527a7aff802b7
Younggeun97 <ygo9734@gmail.com>
2021-11-25 00:07:17 +0900

    unversioned files

--- app/src/main/java/com/example/withmeapp/MainActivity.kt
+++ app/src/main/java/com/example/withmeapp/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        itm
     }
 }
