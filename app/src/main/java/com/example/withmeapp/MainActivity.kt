151f8fd3eaa0c69405b06f8b0fdd386190c75165
Younggeun97 <ygo9734@gmail.com>
2021-11-29 11:32:59 +0900

    YGO_editing

--- app/src/main/java/com/example/withmeapp/MainActivity.kt
+++ app/src/main/java/com/example/withmeapp/MainActivity.kt
@@ -3,15 +3,35 @@ package com.example.withmeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.withmeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val homeFragment = HomeFragment()
+    private val localFragment = LocalFragment()
+    private val joinFragment = JoinFragment()
+    private val mypageFragment = MypageFragment()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        private lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-
-
+        initBinding()
+        initNavigation()
     }
-}
+    private fun initNavigation() {
+        NavigationUI.setupWithNavController(navi, findNavController(R.id.navigationView))
+    }
+
+    private fun initBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.act_main)
+        binding.lifecycleOwner = this
+    }
+ }
+
+
+
