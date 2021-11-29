a5620c827d2e6e73250b57b7dd8243d075939520
Younggeun97 <ygo9734@gmail.com>
2021-11-29 16:28:12 +0900

    YGO_editing

--- app/src/main/java/com/example/withmeapp/MainActivity.kt
+++ app/src/main/java/com/example/withmeapp/MainActivity.kt
@@ -3,15 +3,23 @@ package com.example.withmeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.withmeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
-
+        initBinding()
+        initNavigation()
+    }
+    private fun initNavigation() {
+        NavigationUI.setupWithNavController(binding.navigationView,findNavController(R.id.navi_host))
+    }
+    private fun initBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
     }
 }
