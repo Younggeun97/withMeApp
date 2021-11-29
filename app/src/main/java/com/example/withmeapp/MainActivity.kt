3d308e9238a1bd779b083e670aee2e92b086c200
Younggeun97 <ygo9734@gmail.com>
2021-11-29 17:05:39 +0900

    YGO_editing

--- app/src/main/java/com/example/withmeapp/MainActivity.kt
+++ app/src/main/java/com/example/withmeapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.withmeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -21,5 +23,17 @@ class MainActivity : AppCompatActivity() {
     private fun initBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
+    }
+
+    var lastTimeBackPressed : Long = 0
+    override fun onBackPressed() {
+        if(System.currentTimeMillis() - lastTimeBackPressed >= 1500){
+            lastTimeBackPressed = System.currentTimeMillis()
+            Toast.makeText(this,"'뒤로' 버튼을 한번 더 누르시면 종료됩니다.",Toast.LENGTH_LONG).show() }
+        else {
+            ActivityCompat.finishAffinity(this)
+            System.runFinalization()
+            System.exit(0)
+        }
     }
 }
