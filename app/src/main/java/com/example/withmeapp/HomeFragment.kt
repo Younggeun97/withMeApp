1898bdd1b8efb70170d23ebf3738d93a1fd8df91
Younggeun97 <ygo9734@gmail.com>
2021-12-04 14:36:18 +0900

    YGO_editing

--- app/src/main/java/com/example/withmeapp/HomeFragment.kt
+++ app/src/main/java/com/example/withmeapp/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -17,6 +18,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import com.example.withmeapp.databinding.FragmentHomeBinding
 import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -31,10 +33,12 @@ private const val ARG_PARAM2 = "param2"
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    val handler = Handler()
+    var timeValue = -1
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -46,21 +50,64 @@ class HomeFragment : Fragment() {
     }
 
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
-        binding.maincompass.setOnClickListener{
+
+
+        binding.maincompass.setOnClickListener {
             val intent = Intent(getActivity(), MapsActivity::class.java)
             startActivity(intent)
         }
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+//        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val runnable = object : Runnable {
+            override fun run() {
+                timeValue++
+                //TextView 업데이트 하기
+                timeToText(timeValue)?.let {
+                    timeView.text = it
+                }
+                handler.postDelayed(this, 1000)
+            }
+        }
+
+        //버튼 이벤트 처리
+        binding.startbtn.setOnClickListener {
+            handler.post(runnable)
+        }
+
+        binding.stopbtn.setOnClickListener {
+            handler.removeCallbacks(runnable)
+        }
+
+        binding.resetbtn.setOnClickListener {
+            handler.removeCallbacks(runnable)
+            timeValue = -1
+            timeToText()?.let {
+                timeView.text = it
+            }
+        }
+        return binding.root
+    }
+
+    //변환작업
+    private fun timeToText(time: Int = 0): String? {
+        return if (time < 0) {
+            null
+        } else if (time == 0) {
+            "00:00:00"
+        } else {
+            val h = time / 3600
+            val m = time % 3600 / 60
+            val s = time % 60
+            "%1$02d:%2$02d:%3$02d".format(h, m, s)
+        }
     }
 
     companion object {
