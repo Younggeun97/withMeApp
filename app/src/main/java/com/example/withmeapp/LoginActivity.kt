6d5dc8132f79863759395a162548bb30b2a76684
Younggeun97 <ygo9734@gmail.com>
2021-12-11 01:49:52 +0900

    YGO_editing

--- app/src/main/java/com/example/withmeapp/LoginActivity.kt
+++ app/src/main/java/com/example/withmeapp/LoginActivity.kt
@@ -1,13 +1,13 @@
 package com.example.withmeapp
 
+import android.content.ContentValues
 import android.content.Intent
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.withmeapp.databinding.ActivityLoginBinding
 
 import com.google.android.gms.auth.api.Auth
@@ -15,31 +15,31 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
-import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 import java.util.*
 
 class LoginActivity : AppCompatActivity() {
-    private var auth: FirebaseAuth? = null
-    private var googleSignInClient: GoogleSignInClient? = null
-    private var GOOGLE_LOGIN_CODE = 9001
-
+    var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         auth = FirebaseAuth.getInstance()
 
-
-        // 구글 로그인 버튼
-        binding.logingoogle.setOnClickListener { googleLogin() }
-
-
+        binding.logingoogle.setOnClickListener {
+            //First step
+            googleLogin()
+        }
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("203273459612-n47f6um6u28smldvjh49rm3ap0ho399q.apps.googleusercontent.com")
             .requestEmail()
@@ -47,65 +47,22 @@ class LoginActivity : AppCompatActivity() {
         googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
-//    private fun getHashKey() {
-//        var packageInfo: PackageInfo? = null
-//        try {
-//            packageInfo =
-//                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-//        } catch (e: PackageManager.NameNotFoundException) {
-//            e.printStackTrace()
-//        }
-//        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
-//        for (signature in packageInfo!!.signatures) {
-//            try {
-//                val md = MessageDigest.getInstance("SHA")
-//                md.update(signature.toByteArray())
-//                Log.d(
-//                    "KeyHash",
-//                    Base64.encodeToString(md.digest(), Base64.DEFAULT)
-//                )
-//            } catch (e: NoSuchAlgorithmException) {
-//                Log.e(
-//                    "KeyHash",
-//                    "Unable to get MessageDigest. signature=$signature",
-//                    e
-//                )
-//            }
-//        }
-//    }
-
-    // 로그아웃하지 않을 시 자동 로그인 , 회원가입시 바로 로그인 됨
-    public override fun onStart() {
-        super.onStart()
-        moveMainPage(auth?.currentUser)
-    }
-
-    // 로그인
-    private fun signIn(email: String, password: String) {
-
-        if (email.isNotEmpty() && password.isNotEmpty()) {
-            auth?.signInWithEmailAndPassword(email, password)
-                ?.addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(
-                            baseContext, "로그인에 성공 하였습니다.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        moveMainPage(auth?.currentUser)
-                    } else {
-                        Toast.makeText(
-                            baseContext, "로그인에 실패 하였습니다.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-        }
-    }
-
-    // 구글 로그인 함수
     fun googleLogin() {
         var signInIntent = googleSignInClient?.signInIntent
         startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            // result가 성공했을 때 이 값을 firebase에 넘겨주기
+            if (result!!.isSuccess) {
+                var account = result.signInAccount
+                // Second step
+                firebaseAuthWithGoogle(account)
+            }
+        }
     }
 
     fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
@@ -113,36 +70,18 @@ class LoginActivity : AppCompatActivity() {
         auth?.signInWithCredential(credential)
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    // 아이디, 비밀번호 맞을 때
+                    // Login, 아이디와 패스워드가 맞았을 때
+                    Toast.makeText(this, "success", Toast.LENGTH_LONG).show()
                     moveMainPage(task.result?.user)
                 } else {
-                    // 틀렸을 때
-                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                    // Show the error message, 아이디와 패스워드가 틀렸을 때
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 }
             }
     }
 
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-
-        if (requestCode == GOOGLE_LOGIN_CODE) {
-            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)!!
-            // 구글API가 넘겨주는 값 받아옴
-
-            if (result.isSuccess) {
-                var accout = result.signInAccount
-                firebaseAuthWithGoogle(accout)
-                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-
-    // 유저정보 넘겨주고 메인 액티비티 호출
+    // 로그인이 성공하면 다음 페이지로 넘어가는 함수
     fun moveMainPage(user: FirebaseUser?) {
         if (user != null) {
             startActivity(Intent(this, MainActivity::class.java))
